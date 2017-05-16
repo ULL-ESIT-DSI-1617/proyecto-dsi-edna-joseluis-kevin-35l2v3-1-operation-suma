001e8b6f8687db833fdbeee3f1825408a8c80ae7
ednagc <ednagc.11@gmail.com>
2017-05-16 21:20:38 +0100

    Automatizando tareas. Gulp closes #4

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+gulp.task('default', ['test']);
+
+gulp.task('debug', shell.task('node --inspect --debug-brk index.js'));
+
+gulp.task('run', shell.task('node index.js'));
+
+gulp.task("doc", shell.task("npm run doc"));
