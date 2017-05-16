6800a9e46b66ea4879b003a4bc343d72a99819f0
ednagc <ednagc.11@gmail.com>
2017-05-16 22:15:23 +0100

    Añadiendo prueba mocha chai closes #2

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,5 @@ gulp.task('debug', shell.task('node --inspect --debug-brk index.js'));
 gulp.task('run', shell.task('node index.js'));
 
 gulp.task("doc", shell.task("npm run doc"));
+
+gulp.task("test", shell.task("npm test"));
